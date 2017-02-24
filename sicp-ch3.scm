2f613d174ecdbc142856f8aec243daccd4f03343
Akiyoshi Tsuchida <tsuchiaki2012@gmail.com>
2017-02-24 18:45:59 +0900

    更新

--- sicp-ch3.scm
+++ sicp-ch3.scm
@@ -45,3 +45,27 @@
   dispatch)
 
 
+(define rand
+  (let ((x random-init))
+    (lambda()
+      (set! x (rand-update x))
+      x)))
+
+
+(define (estimate-pi trials)
+  (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+
+(define (cesaro-test)
+  (= (gcd (rand) (rand)) 1))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaning trials-passed)
+    (cond ((= trials-remaning 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaning 1) (+ trials-passed 1)))
+          (else
+            (iter (- trials-remaning 1) trials-passed))))
+  (iter trials 0))
+
+
