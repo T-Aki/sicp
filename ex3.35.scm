d226ebbc34853ebaa8855a74e6c09b386f513be5
a_tsuchida <tsuchiaki2012@gmail.com>
2017-06-04 17:32:22 +0900

    add ex3.35

---
+++ ex3.35.scm
@@ -0,0 +1,22 @@
+;ex3.35.scm
+
+(define (squarer a b)
+  (define (process-new-value)
+    (if (has-value? b)
+        (if (< (get-value b) 0)
+            (error "square less than 0 --SQUARE" (get-value b))
+            (set-value! a (sqrt b) me))
+        (set-value! b (square a) me)))
+  (define (process-forget-value)
+    (forget-value! a me)
+    (forget-value! b me))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value)
+           (process-new-value))
+          ((eq? request 'I-lost-my-value)
+           (process-forget-value))
+          (else
+            (error "Unknown request --SQUARER" request))))
+  (connect a me)
+  (connect b me)
+  me)
