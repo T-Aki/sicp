3864c2d9c055f1db721052f36672fdb1f2a7229c
a_tsuchida <tsuchiaki2012@gmail.com>
2017-06-28 20:02:58 +0900

    update ex3.38

--- ex3.38.scm
+++ ex3.38.scm
@@ -4,11 +4,11 @@ a
 pe pa m 45
 pe m pa 35
 pa pe m 45
-pa m oe 35
+pa m pe 50
 m pe pa 40
 m pa pe 40
 
-ans 35, 40, 45
+ans 35, 40, 45,50
 
 b
 pe 110
@@ -21,4 +21,4 @@ m,pe 60
 pa, m 45
 m,pa 30
 
-ans 30,50,55,60,80,110
+ans 30,55,60,80,110
