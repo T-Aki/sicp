f1c00cc7bb7f9e80a0a7147971daa158d8fce2fd
a_tsuchida <tsuchiaki2012@gmail.com>
2017-06-28 18:34:34 +0900

    add ex3.38

---
+++ ex3.38.scm
@@ -0,0 +1,24 @@
+;ex3.38.scm
+
+a
+pe pa m 45
+pe m pa 35
+pa pe m 45
+pa m oe 35
+m pe pa 40
+m pa pe 40
+
+ans 35, 40, 45
+
+b
+pe 110
+pa 80
+m 50
+
+pe,pa 90
+pe,m 55
+m,pe 60
+pa, m 45
+m,pa 30
+
+ans 30,50,55,60,80,110
