ca46831564a88297e9c9c19e241821771a98665f
a_tsuchida <tsuchiaki2012@gmail.com>
2017-07-04 22:20:11 +0900

    update ch3

--- sicp-ch3.scm
+++ sicp-ch3.scm
@@ -543,5 +543,31 @@
 (define (connect connector new-constraint)
   ((connector 'connect) new-constraint))
 
+;3.42
+(define (make-serializer)
+  (let ((mutex (make-mutex)))
+    (lambda (p)
+      (define (serializer-p . args)
+        (mutex 'acquire)
+        (let ((val (apply p args)))
+          val))
+      serializer-p)))
 
+(define (make-mutex)
+  (let ((cell (list false)))            
+    (define (the-mutex m)
+      (cond ((eq? m 'acquire)
+             (if (test-and-set! cell)
+                 (the-mutex 'acquire))) ; retry
+            ((eq? m 'release) (clear! cell))))
+    the-mutex))
+
+(define (clear! cell)
+  (set-car! cell false))
+
+(define (test-and-set! cell)
+  (if (car cell)
+      true
+      (begin (set-car! cell true)
+             false)))
 
