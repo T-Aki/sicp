acceaa3841176219f175cdb8649e61bc5398d748
a_tsuchida <a_tsuchida@timedia.co.jp>
2017-08-22 15:01:39 +0900

    update ch3

--- sicp-ch3.scm
+++ sicp-ch3.scm
@@ -574,3 +574,65 @@
 
 ;3.5.2
 
+(define (integer-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1)))
+
+(define integers (integers-starting-from 1))
+
+(define (divisible? x y) (= (remainder x y) 0))
+
+(define no-sevens
+  (stream-filter (lambda (x) (not (divisible? x 7)))
+    integers))
+
+(define (sieve stream)
+  (cons-stream
+    (sieve (stream-filter
+      (lambda (x)
+        (not (divisible? x (stream-car stream))))
+      (stream-cdr stream)))))
+
+(define primes (sieve (integers-starting-from 2)))
+
+(define ones (cons-stream 1 ones))
+
+(define (add-streams s1 s2)
+  (dtream-map + s1 s2))
+
+(define integers (cons-stream 1 (add-streams ones integers)))
+
+(defina fibs
+  (cons-stream 0
+    (cons-stream 1
+      (add-streams (stream-cdr fibs)
+        fibs))))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+
+(define double (cons-stream 1 (scale-stream double 2)))
+
+(define primes
+  (cons-stream
+    2
+    (stream-filter prime? (integers-starting-from 3))))
+
+(define (prime? n)
+  (define (iter ps)
+    (cond ((> (square (stream-car ps)) n) #t)
+      (divisible? n (stream-car ps) #f)
+      (else (iter (stream-cdr ps)))))
+  (iter primes))
+
+
+
+
+
+
+
+
+
+
+
+
+
