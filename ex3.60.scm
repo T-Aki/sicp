0b0cf6ad950bcc97287d429aab7d433b3e95de21
a_tsuchida <a_tsuchida@timedia.co.jp>
2017-08-22 16:45:01 +0900

    add ex3.60

---
+++ ex3.60.scm
@@ -0,0 +1,18 @@
+;ex3.60.scm
+(define (mul-series s1 s2)
+	(cons-stream (* (stream-car s1) (stream-car s2))
+		(add-streams (scale-stream (strem-cdr s2) (stream-car s1))
+			(mul-series (stream-cdr s1) s2))))
+
+(stream-ref (add-streams (mul-series sin-series sin-series)
+	(mul-series cosine-series cosine-series)) 0)
+(stream-ref (add-streams (mul-series sin-series sin-series)
+	(mul-series cosine-series cosine-series)) 1)
+(stream-ref (add-streams (mul-series sin-series sin-series)
+	(mul-series cosine-series cosine-series)) 2)
+(stream-ref (add-streams (mul-series sin-series sin-series)
+	(mul-series cosine-series cosine-series)) 3)
+(stream-ref (add-streams (mul-series sin-series sin-series)
+	(mul-series cosine-series cosine-series)) 4)
+(stream-ref (add-streams (mul-series sin-series sin-series)
+	(mul-series cosine-series cosine-series)) 5)
