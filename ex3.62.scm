42a52aaf9b6f3a0db48fed694e28968bae67eb15
a_tsuchida <tsuchiaki2012@gmail.com>
2017-08-23 20:11:07 +0900

    fix ex3.62

--- ex3.62.scm
+++ ex3.62.scm
@@ -3,4 +3,7 @@
 (define (div-series s1 s2)
 	(if (= (stream-car s2) 0)
 		(error "divided by 0")
-		(mul-series s1 (invert-unit-series s2)))
+		(mul-series s1 
+              (invert-unit-series (scale-steram s2 (/ 1 (stream-car s2))))))
+ 
+ 
