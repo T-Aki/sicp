9bc82cdfee2d74e71aebe9e856736da47d2281e2
a_tsuchida <tsuchiaki2012@gmail.com>
2017-09-03 21:42:32 +0900

    add ex3.67

---
+++ ex3.67.scm
@@ -0,0 +1,13 @@
+;ex3.67.scm
+
+(define (pairs s t)
+  (cons-stream
+    (list (stream-car s) (stream-car t))
+      (interleave
+        (interleave
+            (stream-map (lambda (x) (list (stream-car s) x))
+              (stream-cdr t))
+            (pairs (stream-cdr s) (stream-cdr t)))
+        (stream-map (lambda (x) (list x (stream-car t)))
+            (stream-cdr s)))))
+
