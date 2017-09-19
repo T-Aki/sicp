45c0ae5530451d71f3f934071da84bc2b5fdd7f5
a_tsuchida <tsuchiaki2012@gmail.com>
2017-09-19 11:12:16 +0900

    add ex3.77-79

--- sicp-ch3.scm
+++ sicp-ch3.scm
@@ -672,3 +672,18 @@
       (add-streams (scale-stream integrand dt)
         int)))
   int)
+
+
+(define (integral delayed-integrand initial-value dt)
+  (define int
+    (cons-stream initial-value
+      (let ((integrand (force delayed-integrand)))
+        (add-streams (scale-stream integrand dt)
+            int))))
+int)
+
+
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (stream-map f y))
+y)
