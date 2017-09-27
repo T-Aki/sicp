74354f14f7cb22cefeed43e3f1f1c98c05ca8562
a_tsuchida <tsuchiaki2012@gmail.com>
2017-09-27 18:21:40 +0900

    update ex4.1

--- ch4/ex4.1.scm
+++ ch4/ex4.1.scm
@@ -1,2 +1,14 @@
 (load "./4.1")
 
+;左から右へ評価
+(define (list-of-values exps env)
+    (if (no-operands? exps)
+        '()
+        ))
+
+;右から左へ評価
+
+(define (list-of-values exps env)
+    (if (no-operands? exps)
+        '()
+        ))
