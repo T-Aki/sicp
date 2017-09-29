43a8092e01ee4180a44478f4b1ebff8df7b64f00
a_tsuchida <tsuchiaki2012@gmail.com>
2017-09-29 21:44:17 +0900

    コード整形

--- ch4/ex4.1.scm
+++ ch4/ex4.1.scm
@@ -2,13 +2,13 @@
 
 ;左から右へ評価
 (define (list-of-values exps env)
-    (if (no-operands? exps)
-        '()
-        ))
+	(if (no-operands? exps)
+		'()
+		))
 
 ;右から左へ評価
 
 (define (list-of-values exps env)
-    (if (no-operands? exps)
-        '()
-        ))
+	(if (no-operands? exps)
+		'()
+		))
