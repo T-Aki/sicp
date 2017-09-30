90477a9c272a9f6586df6b1f71c998faace0f32d
a_tsuchida <tsuchiaki2012@gmail.com>
2017-09-30 18:08:35 +0900

    update 4.1.scm

--- ch4/4.1.scm
+++ ch4/4.1.scm
@@ -66,3 +66,29 @@
   'ok)
 
 
+(define (self-evaluating? exp)
+  (cond ((number? exp) true)
+        ((string? exp) true)
+        (else false)))
+
+(define (variable? exp) (symbol? exp))
+
+(define (quoted? exp)
+  (tagged-list? exp 'quote))
+
+
+(define (text-of-quotation exp) (cadr exp))
+
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+
+
+(define (assignment? exp)
+  (tagged-list? exp 'set!))
+
+
+
+
