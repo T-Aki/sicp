42c567a09c8329037333a53ef906c167a056da1c
a_tsuchida <tsuchiaki2012@gmail.com>
2017-10-10 11:57:24 +0900

    add ex4.1

--- ch4/ex4.1.scm
+++ ch4/ex4.1.scm
@@ -2,13 +2,23 @@
 
 ;左から右へ評価
 (define (list-of-values exps env)
-	(if (no-operands? exps)
-		'()
-		))
+  (if (no-operands? exps)
+      '()
+      (let (left-first (eval (first-exp exps) env))
+        (cons left-first list-of-values (rest-exp exps) env))))
 
 ;右から左へ評価
 
 (define (list-of-values exps env)
 	(if (no-operands? exps)
-		'()
-		))
+     '()
+     (let (right-first (list-of-values ( rest-exp exps) env))
+       (cons (eval (first-exp exps) env)
+             right-first))))
+
+
+(define val 10)
+
+(define exp '((set! val (+ val 2)) (set! val (* val 2))))
+
+(list-of-values exp val)
