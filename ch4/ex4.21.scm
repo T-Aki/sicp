eff3f77f10e2c850890eda2ef267c9561598bd35
a_tsuchida <tsuchiaki2012@gmail.com>
2017-12-30 11:09:48 +0900

    update ex4.12

--- ch4/ex4.21.scm
+++ ch4/ex4.21.scm
@@ -1,9 +1,9 @@
+;a
 ((lambda (n)
    ((lambda (fib)
       (fib fib n))
     (lambda (fi k)
       (cond ((= k 0) 0)
             ((= k 1) 1)
-            ((= k 2) 1)
             (else (+ (fi fi (- k 1)) (fi fi (- k 2))))))))
  10)
