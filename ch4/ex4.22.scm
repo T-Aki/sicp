247c7268ccdcdf92ef78da0c1598e1130c18bc09
a_tsuchida <tsuchiaki2012@gmail.com>
2017-12-31 15:46:52 +0900

    add ex4.22

---
+++ ch4/ex4.22.scm
@@ -0,0 +1,17 @@
+(load "./4.1")
+
+(define (analyze exp)
+  (cond ((self-evaluating? exp) 
+         (analyze-self-evaluating exp))
+        ((quoted? exp) (analyze-quoted exp))
+        ((variable? exp) (analyze-variable exp))
+        ((assignment? exp) (analyze-assignment exp))
+        ((definition? exp) (analyze-definition exp))
+        ((if? exp) (analyze-if exp))
+        ((lambda? exp) (analyze-lambda exp))
+        ((begin? exp) (analyze-sequence (begin-actions exp)))
+        ((cond? exp) (analyze (cond->if exp)))
+        ((application? exp) (analyze-application exp))
+        ((let? expr) (analyze (let->combination expr))) 
+        (else
+         (error "Unknown expression type -- ANALYZE" exp))))
