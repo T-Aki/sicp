264248c26f4b2d460b6ca51552e4bc55624a435f
akiyoshi <akiyoshi@users.noreply.github.com>
2018-01-13 17:07:14 +0900

    add ex4.27

--- ch4/4.2.scm
+++ ch4/4.2.scm
@@ -99,3 +99,61 @@
         ((evaluated-thunk? obj)
          (thunk-value obj))
         (else obj)))
+
+(define (actual-value exp env)
+  (force-it (eval exp env)))
+
+
+(define (apply procedure arguments env)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure
+          procedure
+          (list-of-arg-values arguments env)))  ; 変更した
+        ((compound-procedure? procedure)
+         (eval-sequence
+          (procedure-body procedure)
+          (extend-environment
+           (procedure-parameters procedure)
+           (list-of-delayed-args arguments env) ; 変更した
+           (procedure-environment procedure))))
+        (else
+         (error
+          "Unknown procedure type -- APPLY" procedure))))
+
+
+(define (list-of-arg-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (actual-value (first-operand exps) env)
+            (list-of-arg-values (rest-operands exps)
+                                env))))
+
+
+(define (list-of-delayed-args exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (delay-it (first-operand exps) env)
+            (list-of-delayed-args (rest-operands exps)
+                                  env))))
+
+(define (eval-if exp env)
+  (if (true? (actual-value (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternative exp) env)))
+
+(define input-prompt ";;; L-Eval input:")
+(define output-prompt ";;; L-Eval value:")
+
+
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output
+           (actual-value input the-global-environment)))
+      (announce-output output-prompt)
+      (user-print output)))
+  (driver-loop))
+
+
+
+(define the-global-environment (setup-environment))
---
+++ ch4/ex4.27.scm
@@ -0,0 +1,16 @@
+ (define w (id (id 10))) 
+  
+ ;;; L-Eval input: 
+ count 
+ ;;; L-Eval value: 
+ 0 
+ Defining w doesn't evaluate it. 
+ ;;; L-Eval input: 
+ w 
+ ;;; L-Eval value: 
+ 10 
+ now that w is in the prompt, w is forced to evaluate, evaluating both ids. so now w = 10, count = 2. 
+ ;;; L-Eval input: 
+ count 
+ ;;;; L-Eval value: 
+ 2 
